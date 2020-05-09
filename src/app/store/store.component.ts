193c01c8fe261240c39a43561897ad02218a89fd
Isha Sharma <isha.sharma01@student.csulb.edu>
2020-05-08 22:24:31 -0700

    my commit

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -29,7 +29,7 @@ export class StoreComponent {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage
     return this.repository
       .getProducts(this.selectedCategory)
-      .filter((product) => this.abc(product.name.toLowerCase()))
+      //.filter((product) => this.abc(product.name.toLowerCase()))
       .slice(pageIndex, pageIndex + this.productsPerPage)
   }
 
