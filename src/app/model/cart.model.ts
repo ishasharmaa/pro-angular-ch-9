e2ad38cbfa89168c3e77fbce6b68884997053619
Isha Sharma <isha.sharma01@student.csulb.edu>
2020-05-08 22:26:33 -0700

    my commit

--- src/app/model/cart.model.ts
+++ src/app/model/cart.model.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
 import { Product } from './product.model'
 
-@Injectable() // inject it to other places
+@Injectable() 
 export class Cart {
   public lines: CartLine[] = []
   public itemCount: number = 0
